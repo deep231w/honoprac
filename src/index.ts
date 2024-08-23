2b5ee80578e577bf67e57770f355c44e67fd4d8d
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-08-23 17:11:10 +0530

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,72 @@
-import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono';
+import { sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+ const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string
+  }
+}>();
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+// Create the main Hono app
+// const app = new Hono<{
+// 	Bindings: {
+// 		DATABASE_URL: string,
+// 		JWT_SECRET: string,
+// 	}
+// }>();
+
+app.post('/api/v1/signup', async(c) => {
+    const prisma =new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json()
+    try {
+          const user = await prisma.user.create( { 
+           data:{
+              email: body.email,
+              password: body.password,
+            }}
+        )
+        console.log('JWT_SECRET:', c.env.JWT_SECRET);
+
+        const token = await sign({id:user.id}, c.env.JWT_SECRET);
+        console.log(token);
+        return c.json({ token })
+           
+    }catch (e) {
+      console.error("error:", e);
+      return c.json({message: 'Error creating'})
+    }
 })
 
-app.post('/api/v1/login', (c) => {
-  return c.text('Hello Hono!')
+
+
+app.post('/api/v1/signin', async (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+      const user = await prisma.user.findUnique({
+        where: { 
+          email: body.email
+         }
+      })
+      if (!user) {
+        return c.json({ message: 'User not found' })
+      }
+      console.log('JWT_SECRET', c.env.JWT_SECRET)
+      const token = await sign({id: user.id}, c.env.JWT_SECRET);
+      return c.json({ token })
+  }catch (e) {
+    console.error(e)
+    return c.json({ message: 'Error logging in' })
+  }
 })
 
 app.get('/api/v1/blog/:id', (c) => {
