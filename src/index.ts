7c400538ea19cd7cebe65786d88216c14025f8a5
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-24 23:27:31 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono';
 import { sign, verify } from 'hono/jwt'
-
+ 
  const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
